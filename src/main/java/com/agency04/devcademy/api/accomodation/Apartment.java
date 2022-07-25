b04903d6c4d313879afef847c91fcf47d00fbf6d
Ante Milina <ante.milina@hotmail.com>
2022-07-25 11:12:06 +0200

    Add class Apartment

---
+++ src/main/java/com/agency04/devcademy/api/accomodation/Apartment.java
@@ -0,0 +1,22 @@
+package com.agency04.devcademy.api.accomodation;
+
+public class Apartment {
+
+    public Apartment() {
+    }
+
+    @Override
+    public int hashCode() {
+        return super.hashCode();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        return super.equals(obj);
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+}
