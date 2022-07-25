73419fb26dc28c299491362cdaa66c78de8bac48
Ante Milina <ante.milina@hotmail.com>
2022-07-25 11:09:39 +0200

    Add class Accomodation

---
+++ src/main/java/com/agency04/devcademy/api/accomodation/Accommodation.java
@@ -0,0 +1,98 @@
+package com.agency04.devcademy.api.accomodation;
+
+import javax.validation.constraints.Size;
+import java.net.URL;
+
+public class Accommodation {
+    @Size(max = 100)
+    private String title;
+    @Size(max = 200)
+    private String subtitle;
+    private String description;
+    @Size(min = 1, max = 5)
+    private Integer categorization;
+    @Size(min = 1)
+    private Integer personCount;
+    private URL imageUrl;
+    private boolean freeCancelation = true;
+    private double price;
+
+    public enum Type {ROOM, APARTMENT, MOBILE_HOME}
+
+    public Accommodation(){}
+
+    public Accommodation(String title, String subtitle, String description, Integer categorization, Integer personCount, URL imageUrl, boolean freeCancelation, double price) {
+        this.title = title;
+        this.subtitle = subtitle;
+        this.description = description;
+        this.categorization = categorization;
+        this.personCount = personCount;
+        this.imageUrl = imageUrl;
+        this.freeCancelation = freeCancelation;
+        this.price = price;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getSubtitle() {
+        return subtitle;
+    }
+
+    public void setSubtitle(String subtitle) {
+        this.subtitle = subtitle;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Integer getCategorization() {
+        return categorization;
+    }
+
+    public void setCategorization(Integer categorization) {
+        this.categorization = categorization;
+    }
+
+    public Integer getPersonCount() {
+        return personCount;
+    }
+
+    public void setPersonCount(Integer personCount) {
+        this.personCount = personCount;
+    }
+
+    public URL getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(URL imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    public boolean isFreeCancelation() {
+        return freeCancelation;
+    }
+
+    public void setFreeCancelation(boolean freeCancelation) {
+        this.freeCancelation = freeCancelation;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+}
