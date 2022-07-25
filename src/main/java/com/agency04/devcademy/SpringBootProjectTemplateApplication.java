2a40597b193841292a09fd36e15269465c5f9a25
Ante Milina <ante.milina@hotmail.com>
2022-07-25 22:01:23 +0200

    Add 3 accommodation instances

--- src/main/java/com/agency04/devcademy/SpringBootProjectTemplateApplication.java
+++ src/main/java/com/agency04/devcademy/SpringBootProjectTemplateApplication.java
@@ -3,6 +3,7 @@ package com.agency04.devcademy;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class SpringBootProjectTemplateApplication {
 
--- src/main/java/com/agency04/devcademy/bootstrap/BootStrapData.java
+++ src/main/java/com/agency04/devcademy/bootstrap/BootStrapData.java
@@ -6,6 +6,7 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
 import java.net.URL;
+import java.util.*;
 
 @Component
 public class BootStrapData implements CommandLineRunner {
@@ -18,10 +19,27 @@ public class BootStrapData implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-        Accommodation accommodation = new Accommodation("title","subtitle", "opis", 3,5,
-                new URL("https://q-xx.bstatic.com/xdata/images/hotel/840x460/95024620.jpg?k=df17293ee20498e7a4bd952c3df0d527eb111fc6ca10a78c5601229eb60752be&o="),
-                true,150);
 
-        accommodationRepository.save(accommodation);
+        List<Accommodation> accommodation = new ArrayList<>() {
+            {
+                add(new Accommodation("title", "subtitle", "opis", 3, 5,
+                        new URL("https://q-xx.bstatic.com/xdata/images/hotel/840x460/95024620.jpg?k=df17293ee20498e7a4bd952c3df0d527eb111fc6ca10a78c5601229eb60752be&o="),
+                        true, 545));
+                add(new Accommodation("title1", "subtitle1", "opis1", 2, 2,
+                        new URL("https://q-xx.bstatic.com/xdata/images/hotel/840x460/95024620.jpg?k=df17293ee20498e7a4bd952c3df0d527eb111fc6ca10a78c5601229eb60752be&o="),
+                        true, 223));
+                add(new Accommodation("title2", "subtitle2", "opis", 1, 10,
+                        new URL("https://q-xx.bstatic.com/xdata/images/hotel/840x460/95024620.jpg?k=df17293ee20498e7a4bd952c3df0d527eb111fc6ca10a78c5601229eb60752be&o="),
+                        false, 860));
+            }
+        };
+        for (Accommodation acc : accommodation) {
+            accommodationRepository.save(acc);
+            System.out.println(acc);
+        }
+
+
+            System.out.println("Number of entries: " + accommodationRepository.count());
+
     }
 }
