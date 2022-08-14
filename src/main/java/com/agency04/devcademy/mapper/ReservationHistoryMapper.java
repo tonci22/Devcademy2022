6d4f0ecd94d6450688d9e2da62d8477b1214f71c
Ante Milina <ante.milina@hotmail.com>
2022-08-14 15:46:40 +0200

    Bugfix entryTimestamp so it doesn't change on every call

--- src/main/java/com/agency04/devcademy/mapper/ReservationHistoryMapper.java
+++ src/main/java/com/agency04/devcademy/mapper/ReservationHistoryMapper.java
@@ -5,13 +5,9 @@ import com.agency04.devcademy.domain.ReservationHistory;
 import com.agency04.devcademy.dto.request.ReservationHistoryCreateDto;
 import com.agency04.devcademy.dto.request.ReservationUpdateDto;
 import com.agency04.devcademy.dto.response.ReservationHistoryDtoResponse;
-import org.hibernate.annotations.CreationTimestamp;
-import org.mapstruct.BeforeMapping;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 
-import java.sql.Timestamp;
-import java.util.Date;
 import java.util.List;
 
 @Mapper(componentModel = "spring")
@@ -21,14 +17,7 @@ public interface ReservationHistoryMapper {
     @Mapping(target = "idReservation", source = "reservation.id")
     @Mapping(target = "fromType", source = "reservation.type")
     @Mapping(target = "toType", source = "reservationUpdateDto.type")
-    @Mapping(target = "entryTimestamp", ignore = true)
     ReservationHistory mapToReservationHistory(Reservation reservation, ReservationUpdateDto reservationUpdateDto);
-
-    @BeforeMapping
-    default void timestampNow(ReservationHistory reservationHistory) {
-        Timestamp timestamp = new Timestamp(new Date().getTime());
-        reservationHistory.setEntryTimestamp(timestamp);
-    }
 
     ReservationHistoryDtoResponse mapToDto(ReservationHistory reservationHistory);
 
